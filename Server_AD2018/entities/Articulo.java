37f2501d21edde9fa3dd5cec83bccb2637fd88dc
pablotese <pablostrejo@gmail.com>
2018-05-09 19:52:45 -0300

    proyectos

---
+++ Server_AD2018/entities/Articulo.java
@@ -0,0 +1,31 @@
+package entities;
+
+public class Articulo {
+	
+	private int idArticulo;
+	
+	private String codigoBarras;
+	
+	private String descripcion;
+	
+	private int unidad;
+	
+	private TipoPresentacion presentacion;
+	
+	private String tamanio;
+	
+	private Lote lote;
+	
+	private MovimientoStock movimientos;
+	
+	private Posicion ubicacion;
+	
+	private float precioVenta;
+	
+	private int cantidadCompra;
+	
+	private int cantidadPorUbicacion;
+	
+	
+
+}
